8a43764a31ba53633c64c021ce1d74f5662ed49d
Josué Clément <josue.d.clement@gmail.com>
2025-11-30 23:37:07 +0100

    Fix indent

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,43 +9,43 @@ static class Program
 {
     static void Main()
     {
-var builder = new WixMsiBuilder("MyApp")
-    .SetInstallDir(
-        installPath: Path.Combine("%ProgramFiles%", "MyApp"),
-        releasePath: @"C:\Temp\net472\")
-    .AddShortcut(
-        shortcutPath: "%Desktop%",
-        shortcutName: "Myapp",
-        targetPath: Path.Combine("[INSTALLDIR]", "MyApp.exe"),
-        iconPath: @"C:\Temp\net472\logo.ico")
-    .AddShortcut(
-        shortcutPath: "%ProgramMenu%",
-        shortcutName: "Myapp",
-        targetPath: Path.Combine("[INSTALLDIR]", "MyApp.exe"),
-        iconPath: @"C:\Temp\net472\logo.ico")
-    .SetInstallScope(InstallScope.perMachine)
-    .SetAppVersion("1.1.0")
-    .SetProductId(Guid.Parse("f9af10ed-ea36-424b-973e-a0e651203370"))
-    .SetUpgradeCode(Guid.Parse("e74fb99c-5bf1-454d-802a-c7c6f9cc219f"))
-    .SetControlPanelInfo(productInfo =>
-    {
-        productInfo.Manufacturer = "YourCompany";
-        productInfo.ProductIcon = @"C:\Temp\net472\logo.ico";
-        productInfo.Comments = "My awesome app blah blah blah";
-        productInfo.Contact = "support@yourcompany.com";
-        productInfo.HelpLink = "https://www.yourcompany.com/support";
-        productInfo.UrlInfoAbout = "https://www.yourcompany.com/about";
-    })
-    .SetCompressionLevel(CompressionLevel.none)
-    .SetOutput(
-        outputPath: @"C:\Temp\",
-        msiFilename: "MyInstaller");
-
-var (success, path, message) = builder.BuildMsi();
-if (success)
-    Console.WriteLine($"Installer created at {path}");
-else
-    Console.WriteLine($"Error building installer: {message}");
+        var builder = new WixMsiBuilder("MyApp")
+            .SetInstallDir(
+                installPath: Path.Combine("%ProgramFiles%", "MyApp"),
+                releasePath: @"C:\Temp\net472\")
+            .AddShortcut(
+                shortcutPath: "%Desktop%",
+                shortcutName: "Myapp",
+                targetPath: Path.Combine("[INSTALLDIR]", "MyApp.exe"),
+                iconPath: @"C:\Temp\net472\logo.ico")
+            .AddShortcut(
+                shortcutPath: "%ProgramMenu%",
+                shortcutName: "Myapp",
+                targetPath: Path.Combine("[INSTALLDIR]", "MyApp.exe"),
+                iconPath: @"C:\Temp\net472\logo.ico")
+            .SetInstallScope(InstallScope.perMachine)
+            .SetAppVersion("1.1.0")
+            .SetProductId(Guid.Parse("f9af10ed-ea36-424b-973e-a0e651203370"))
+            .SetUpgradeCode(Guid.Parse("e74fb99c-5bf1-454d-802a-c7c6f9cc219f"))
+            .SetControlPanelInfo(productInfo =>
+            {
+                productInfo.Manufacturer = "YourCompany";
+                productInfo.ProductIcon = @"C:\Temp\net472\logo.ico";
+                productInfo.Comments = "My awesome app blah blah blah";
+                productInfo.Contact = "support@yourcompany.com";
+                productInfo.HelpLink = "https://www.yourcompany.com/support";
+                productInfo.UrlInfoAbout = "https://www.yourcompany.com/about";
+            })
+            .SetCompressionLevel(CompressionLevel.none)
+            .SetOutput(
+                outputPath: @"C:\Temp\",
+                msiFilename: "MyInstaller");
+        
+        var (success, path, message) = builder.BuildMsi();
+        if (success)
+            Console.WriteLine($"Installer created at {path}");
+        else
+            Console.WriteLine($"Error building installer: {message}");
         
     }
 }
